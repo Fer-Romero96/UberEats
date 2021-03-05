f60d6a136ac8d6175f97c49cfdf88deba532d26c
Fer-Romero96 <A01335834@itesm.mx>
2021-03-04 22:11:12 -0600

    Se agrego el HomeActivity y se cambio el boton de place Order

---
+++ app/src/main/java/edu/itesm/ubereats/HomeActivity.kt
@@ -0,0 +1,34 @@
+package edu.itesm.ubereats
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Abilita las banderas de la ventana para que no se ponga el titulo ese que tapa
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Un manejador para que espere 3000 milisegundos y haga el intent para cambiar de actividad
+        Handler().postDelayed({
+
+            // Creamos un intento, recibe como parametros donde estamos y a que actividad vamos
+            val intento = Intent(this, MainActivity::class.java)
+
+            // Lanza la actividad
+            startActivity(intento)
+
+            // Termina el ciclo de vida del intento
+            finish()
+        }, 3000)
+    }
+
+}
